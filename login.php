3eeb079d330b36c6ca199b41b76c2ab2ca57d024
alhankeser <alhankeser@gmail.com>
2016-01-31 17:22:19 -0800

    login template

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+include "functions/init.php";
+include "includes/head.php";
+include "includes/main-nav.php";
+?>
+
+<div class="container login-form col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+
+<h1>Login</h1>
+
+<form method="post" class="login-form">
+	<div class="form-group form-group-log">
+		<label class="control-label">Email</label>
+		<input type="email" name="user-email" class="form-control">
+	</div>
+	<div class="form-group form-group-log">
+		<label class="control-label">Password</label>
+		<input type="password" name="user-pw" class="form-control">
+	</div>
+	<button class="btn btn-success btn-block" type="submit" name="login-submit">Login</button>
+
+</form>
+
+</div>
+
+<?php
+include "includes/footer.php";
+include "functions/close.php";
+?>
